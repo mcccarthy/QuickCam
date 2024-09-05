909c7428dd33d10bb70ce8150135ad5e3fe3a662
James Mccarthy <mccarthyjames35@yahoo.com>
2024-09-05 12:51:33 -0700

    added personal code functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,13 @@
 import * as store from './store.js';
 import * as wss from './wss.js';
 
+// initialization of socketIO connection
 const socket = io('/');
 wss.registerSocketEvents(socket);
+
+//register event for personal code button
+const personalCodeCopyButton = document.getElementById('personal_code_copy_button');
+personalCodeCopyButton.addEventListener('click', () => {
+	const personalCode = store.getState().socketId;
+	navigator.clipboard && navigator.clipboard.writeText(personalCode);
+});
