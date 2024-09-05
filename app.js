44871eb17c39507de4edcb82d280931322c41aed
James Mccarthy <mccarthyjames35@yahoo.com>
2024-09-04 19:46:34 -0700

    connected to servers an cl to verify, starting ui

--- app.js
+++ app.js
@@ -5,18 +5,25 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
-
 const io = require('socket.io')(server);
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-	res.send(__dirname + '/public/index.html');
+	res.sendFile(__dirname + '/public/index.html'); // Use res.sendFile for correct file serving
 });
 
+let connectedPeers = [];
+
 io.on('connection', socket => {
-	console.log('a user connected to socket io server');
-	console.log(socket.id);
+	connectedPeers.push(socket.id);
+	console.log(`User connected: ${socket.id}`);
+
+	socket.on('disconnect', () => {
+		console.log(`User disconnected: ${socket.id}`);
+		connectedPeers = connectedPeers.filter(peerSocketId => peerSocketId !== socket.id);
+	});
+	console.log(connectedPeers);
 });
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
